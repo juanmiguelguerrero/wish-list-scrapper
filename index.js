d16363dc7d70866675b53f5c689ba4ec02ab1125
Juan Miguel Guerrero <juanmiguel@existo.es>
2019-11-30 14:10:15 +0100

    Primer prototipo

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const server = express()
+const port = 8080
+
+const productScrapper = require('./scrapper.js')
+
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
+server.get("/", (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
+server.post("/", async (req, res) => {
+	
+	let url = req.body.url
+
+	if (!url) {
+		res.json({ message: "URL parameter not received"})
+	} else {
+		let results = await productScrapper(url)
+		res.json(results)
+	}
+})
+
+server.listen(port, () => {
+	console.log(`Server listening at ${port}`)
+})
---
+++ scrapper.js
@@ -0,0 +1,52 @@
+const request = require('request-promise')
+const cheerio = require('cheerio')
+
+
+// Load HTML from a URL
+async function loadURL(url) {
+	
+	const options = {
+		url: url,
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'			
+		}
+	}
+	try {
+		let html = await request(url)
+		return html
+	} catch {
+		console.error('Error!!!')
+		return "... URL Load Error!!!"
+	}
+}
+
+// Extract product information from Amazon Wish List URL 
+async function productScraper(url) {
+
+	let results = []
+	let html = await loadURL(url)
+	let $ = cheerio.load(html)
+	
+	$('#g-items li').each((i, element) => {
+		
+		let price = $(element).attr('data-price')
+		let name = $(element).find('h3 .a-link-normal').attr('title')
+		let image = $(element).find('.g-itemImage img').attr('src').replace("._SS135_.", "._AC_UY879_.")
+		let params = JSON.parse($(element).attr('data-reposition-action-params'))
+		
+		let ASIN = params.itemExternalId.slice(5, 15)
+		let link = `https://www.amazon.es/dp/${ASIN}`
+
+		results.push({
+			asin: ASIN,
+			name: name,
+			link: link,
+			image: image,
+			price: price
+		})
+	})
+	console.log(results)
+	return results
+}
+
+module.exports = productScraper
