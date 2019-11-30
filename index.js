f9ca3d97ae9717fd61d3a8ff4f7e8e13525a8622
Juan Miguel Guerrero <juanmiguel@existo.es>
2019-11-30 14:39:24 +0100

    Configuración CORS

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const server = express()
+const cors = require('cors')
 const port = process.env.PORT || 8080
 
 const productScrapper = require('./scrapper.js')
 
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
+server.use(cors())
 
 server.get("/", (req, res) => {
 	res.sendFile(__dirname + '/index.html')
